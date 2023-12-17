31d3b5bf969cb9e9dbce14497d51a3d72ea457ac
Aaron Brown <aaron.jacob.brown@gmail.com>
2023-12-16 19:02:04 -0700

    Edited comments

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -294,12 +294,12 @@ elif selected_month2 == 'May':
     st.empty()
 
 
-st.write('Going back to the previous trends depicted in the correlation heatmaps, it is noted that as the months ' \
-         'of the year progress, the observed snowpack (in) distributions shift in opposite directions—specifically, the ' \
-            'distribution from 1979-1999 sees relatively-higher levels of snowpack in the late season. It is interesting ' \
-                'because at the same time, the distribution from 2010-2023 sees relatively-higher levels of snowpack ' \
-                    'in the early season. Therefore, these trends provide evidence that if there is a general decline in snowfall ' \
-                        'over years, it is more likely attributed to fluctuating weather patterns and temperature increases (causing shorter winter seasons) due ' \
+st.write('Going back to the correlation heatmaps, it is noted that as the months ' \
+         'of the year progress, the snowpack (in) distributions by decade shift in opposite directions (also depicted here). ' \
+           'It is interesting because the distribution from 2010-2023 sees relatively-higher levels of snowpack ' \
+            'in the early season, but that is not the case during the spring months. Therefore, these trends provide evidence that ' \
+                'if there is a general decline in snowfall over years, it is more likely attributed to fluctuating weather ' \
+                    'patterns and temperature increases (causing shorter winter seasons) due ' \
                             'to global trends, rather than a long-term drought issue.')
 
 
